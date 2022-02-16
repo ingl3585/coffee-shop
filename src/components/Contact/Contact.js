19283fe8e4d3dd3df2ecec013d0fada8bf06dd7e
ingl3585 <ingleanthonyco@gmail.com>
2022-02-16 12:26:17 -0700

    added contact form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,7 +1,95 @@
+import React, { useState } from 'react';
 import './contact.css';
 
 const Contact = () => {
-	return <div>Contact</div>;
+	const [data, setData] = useState({
+		firstName: '',
+		lastName: '',
+		email: '',
+		message: '',
+	});
+
+	const { firstName, lastName, email, message } = data;
+
+	const handleChange = (event) => {
+		setData({ ...data, [event.target.name]: event.target.value });
+	};
+
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+		try {
+			const response = await fetch(
+				'https://v1.nocodeapi.com/ingl3585/google_sheets/CoYpzKbitMpZbKry?tabId=Sheet1',
+				{
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json',
+					},
+					body: JSON.stringify([
+						[firstName, lastName, email, message, new Date().toLocaleString()],
+					]),
+				}
+			);
+			await response.json();
+			setData({ ...data, firstName: '', lastName: '', email: '', message: '' });
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
+	return (
+		<div className='contact-container'>
+			<div className='about-me-container'>
+				<div className='about-me-title'>About Me</div>
+				<div className='about-me-content-one'>
+					I'm a meteorologist turned full-stack developer based in Denver, CO.
+				</div>
+				<div className='about-me-content-two'>
+					Interested in working together or maybe just want to stop by and say
+					hello?
+				</div>
+				<div className='about-me-content-three'>Send a me a message below!</div>
+			</div>
+			<form className='contact-form-container' onSubmit={handleSubmit}>
+				<div className='contact-title'>Contact Me</div>
+				<input
+					className='first-name-form'
+					type='text'
+					name='firstName'
+					value={firstName}
+					onChange={handleChange}
+					placeholder='First Name'
+				/>
+				<input
+					className='last-name-form'
+					type='text'
+					name='lastName'
+					value={lastName}
+					onChange={handleChange}
+					placeholder='Last Name'
+				/>
+				<input
+					className='email-form'
+					type='text'
+					name='email'
+					value={email}
+					onChange={handleChange}
+					placeholder='Email'
+				/>
+				<input
+					className='message-form'
+					type='text'
+					name='message'
+					value={message}
+					onChange={handleChange}
+					placeholder='Message'
+				/>
+				<button type='submit' className='contact-me-btn'>
+					Submit
+				</button>
+			</form>
+		</div>
+	);
 };
 
 export default Contact;
